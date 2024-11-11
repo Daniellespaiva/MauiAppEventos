b07c066a16b77d1b8a508b24948f0d45db85dfb2
Daniellespaiva <daniellespaiva@outlook.com>
2024-11-11 18:13:16 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppEventos/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿
+namespace MauiAppEventos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Views.CadastroEventos());
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+        }
+
+    }
+}
+
---
+++ MauiAppEventos/Views/CadastroEventos.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppEventos.Views;
+
+public partial class CadastroEventos : ContentPage
+{
+	public CadastroEventos()
+	{
+		InitializeComponent();
+	}
+}
